93d328a45218ae643c9f7bbc74e6b250832a96bd
Shao Zengwei <zengwei19861029@163.com>
2015-04-20 22:04:00 +0800

    add karma-test

---
+++ test/unit-test/user-spec.js
@@ -0,0 +1,16 @@
+describe("user suite test" function(){
+    var user;
+
+    beforeEach(function(){
+        console.log("before each unit test....");
+    });
+
+    afterEach(function(){
+        console.log("after each unit test.....");
+    });
+
+    it("render add user page.", function(){
+    debugger;
+        expect(true).toBe(false);
+    });
+});
